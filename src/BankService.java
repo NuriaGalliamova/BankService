726d7dfcbd0f157046672d0af6f2e52c586a07c7
NuriaGalliamova <ganuriya1992@yandex.ru>
2020-10-15 14:09:32 +0300

    fixed #2

--- src/BankService.java
+++ src/BankService.java
@@ -1,20 +1,19 @@
-public class DepositProfitService {
-
-    int calculate(int deposit, int percent) {
-        int profit = deposit * percent / 100;
-        return profit;
-    }
-
-    int percentUsual = 3;
-    int percentHigh = 5;
-    int percentSpecial = 30;
-    int cashbackLimit = 3000;
+public class BankService {
 
     int cashbackCalculate(int expencesUsual, int expencesHigh, int expencesSpecial) {
+        int percentUsual = 3;
+        int percentHigh = 5;
+        int percentSpecial = 30;
+        int cashbackLimit = 3000;
         int cashback = expencesUsual * percentUsual / 100 + expencesHigh * percentHigh / 100 + expencesSpecial * percentSpecial / 100;
         if (cashback > cashbackLimit) {
             return cashbackLimit;
         }
         return cashback;
+    }
+
+    int profitCalculate(int deposit, int percent) {
+        int profit = deposit * percent / 100;
+        return profit;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        DepositProfitService service = new DepositProfitService();
-        int result = service.calculate(5000, 5);
-        System.out.println(result);
+
+        BankService service = new BankService();
+        int result1 = service.profitCalculate(5000, 5);
+        System.out.println(result1);
+        int result2 = service.cashbackCalculate(10000, 15000, 20000);
+        System.out.println(result2);
+
     }
-}
+
+// код не работает, но я не знаю, в чем тут ошибка
