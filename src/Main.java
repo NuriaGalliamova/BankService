74c12b3fe87b1ad8f003018a41ddc789e85bb221
NuriaGalliamova <ganuriya1992@yandex.ru>
2020-10-16 18:10:20 +0300

    Main.java updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        BankService service = new BankService();
-        int result1 = service.profitCalculate(5000, 5);
+        BankService profit = new BankService();
+        int result1 = profit.profitCalculate(5000, 5);
         System.out.println(result1);
-        int result2 = service.cashbackCalculate(10000, 15000, 20000);
+
+        BankService cashback = new BankService();
+        int result2 = cashback.cashbackCalculate(10000, 15000, 20000);
         System.out.println(result2);
 
     }
+}
 
-// код не работает, но я не знаю, в чем тут ошибка
